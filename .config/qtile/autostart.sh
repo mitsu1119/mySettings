41d0f5d48522e49d770c05dc38a5df31912b8efb
mitsu1119 <hachimitsu1119@gmail.com>
2022-12-12 20:26:22 +0900

    fcitx

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,7 +3,7 @@ feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
 picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # fcitx
-fcitx5 & disown
+fcitx & disown
 
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
